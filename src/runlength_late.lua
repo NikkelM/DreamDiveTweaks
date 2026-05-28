ed173b80ceb7e1eaee100e341a9cc58d6e2a0cba
Aditya Gupta <adi1998.2010@gmail.com>
2026-05-28 16:48:52 +0530

    handle missing scaling data

--- src/runlength_late.lua
+++ src/runlength_late.lua
@@ -26,12 +26,20 @@ end)
 function mod.OpenUpgradeChoiceMenu_NPC(base, source, args)
     if game.CurrentRun.EnteredBiomes > 4 and game.CurrentRun.EnteredBiomes <= 8 then
         for _, item in pairs(source.UpgradeOptions) do
-			item.Rarity = _PLUGIN.guid .. "RarityBiome1"
+            if (game.TraitData[item.ItemName].RarityLevels or {})[_PLUGIN.guid .. "RarityBiome1"] then
+			    item.Rarity = _PLUGIN.guid .. "RarityBiome1"
+            else
+                item.Rarity = "Heroic"
+            end
 		end
     end
     if game.CurrentRun.EnteredBiomes > 8 then
         for _, item in pairs(source.UpgradeOptions) do
-			item.Rarity = _PLUGIN.guid .. "RarityBiome2"
+            if (game.TraitData[item.ItemName].RarityLevels or {})[_PLUGIN.guid .. "RarityBiome2"] then
+			    item.Rarity = _PLUGIN.guid .. "RarityBiome2"
+            else
+                item.Rarity = "Heroic"
+            end
 		end
     end
     return base(source, args)
