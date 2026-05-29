fa60cdd4f752644be41332f6c0f0eac0d9233e5d
Aditya Gupta <adi1998.2010@gmail.com>
2026-05-29 12:01:48 +0530

    workaround for HUD being disabled during Typhon babality phase

--- src/visage_late.lua
+++ src/visage_late.lua
@@ -68,16 +68,6 @@ modutil.mod.Path.Context.Wrap.Static("TyphonHeadSummonPresentation", function ( 
     end)
 end)
 
-modutil.mod.Path.Context.Wrap.Static("FakeDeathTyphonEntrance", function ( unit )
-    modutil.mod.Path.Wrap("SetThingProperty", function (base, args)
-        args = args or {}
-        if game.CurrentRun.IsDreamRun and config.disable_visage_forms.model and args.Property == "GrannyTexture" and string.match(args.Value, "_Color") then
-            args.Value = string.gsub(args.Value, "Dream", "")
-        end
-        return base(args)
-    end)
-end)
-
 modutil.mod.Path.Context.Wrap.Static("TyphonHeadStageTransition", function ( unit )
     modutil.mod.Path.Wrap("SetThingProperty", function (base, args)
         args = args or {}
