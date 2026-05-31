23ce8469818f5ca266f30810c0be85de16796212
Aditya Gupta <adi1998.2010@gmail.com>
2026-05-31 21:06:40 +0530

    remove Plentiful Forage overrides as they are now handled in Resources_In_Chaos_Trials

--- src/harvest.lua
+++ src/harvest.lua
@@ -1,18 +1,3 @@
-game.TraitData.PlantHealthBoon.GameStateRequirements[4] = nil
-
-game.TraitData.PlantHealthBoon.GameStateRequirements.OrRequirements = {
-    {
-        {
-            PathFalse = { "CurrentRun", "IsDreamRun", },
-        },
-    },
-    {
-        {
-            PathTrue = {_PLUGIN.guid, "config", "dream_resources"},
-        }
-    }
-}
-
 modutil.mod.Path.Wrap("CreateRoom", function (base, roomData, args)
     rom.mods["NikkelM-Resources_In_Chaos_Trials"].config.dreamDives = config.dream_resources
     return base(roomData, args)
